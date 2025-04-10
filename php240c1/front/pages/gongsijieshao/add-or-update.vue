233d31dd6bdea2ca51f1d9b89cb3049418433cc9
yulinyu <2508000341@QQ.COM>
2025-04-10 20:13:48 +0800

    1

---
+++ php240c1/front/pages/gongsijieshao/add-or-update.vue
@@ -0,0 +1,388 @@
+<template>
+	<view class="content">
+		<form class="app-update-pv">
+			 
+			<view :style='{"padding":"0 40rpx","boxShadow":"0 0 0px rgba(0,0,0,.3)","margin":"0 0 20rpx 0","borderColor":"#ccc","backgroundColor":"rgba(255, 255, 255, 1)","borderRadius":"0","borderWidth":"0","width":"100%","borderStyle":"solid","height":"108rpx"}' class="cu-form-group">
+				<view :style='{"padding":"0","boxShadow":"0 0 16rpx rgba(0,0,0,0)","margin":"0","borderColor":"#ccc","backgroundColor":"rgba(0,0,0,0)","color":"rgba(161, 161, 161, 1)","textAlign":"center","borderRadius":"0","borderWidth":"0","width":"160rpx","lineHeight":"80rpx","fontSize":"28rpx","borderStyle":"solid"}' class="title">公司名称</view>
+				<input :style='{"padding":"0 30rpx","boxShadow":"0 0 0px rgba(0,0,0,.6) inset","margin":"0","borderColor":"rgba(0,0,0,.6)","backgroundColor":"rgba(255, 255, 255, 0)","color":"rgba(161, 161, 161, 1)","textAlign":"left","borderRadius":"16rpx","borderWidth":"2rpx","width":"calc(100% - 160rpx)","fontSize":"28rpx","borderStyle":"solid","height":"88rpx"}' :disabled="ro.gongsimingcheng" v-model="ruleForm.gongsimingcheng" placeholder="公司名称"></input>
+			</view>
+			<view :style='{"padding":"0 40rpx","boxShadow":"0 0 0px rgba(0,0,0,.3)","margin":"0 0 20rpx 0","borderColor":"#ccc","backgroundColor":"rgba(255, 255, 255, 1)","borderRadius":"0","borderWidth":"0","width":"100%","borderStyle":"solid","height":"108rpx"}' class="cu-form-group">
+				<view :style='{"padding":"0","boxShadow":"0 0 16rpx rgba(0,0,0,0)","margin":"0","borderColor":"#ccc","backgroundColor":"rgba(0,0,0,0)","color":"rgba(161, 161, 161, 1)","textAlign":"center","borderRadius":"0","borderWidth":"0","width":"160rpx","lineHeight":"80rpx","fontSize":"28rpx","borderStyle":"solid"}' class="title">地址</view>
+				<input :style='{"padding":"0 30rpx","boxShadow":"0 0 0px rgba(0,0,0,.6) inset","margin":"0","borderColor":"rgba(0,0,0,.6)","backgroundColor":"rgba(255, 255, 255, 0)","color":"rgba(161, 161, 161, 1)","textAlign":"left","borderRadius":"16rpx","borderWidth":"2rpx","width":"calc(100% - 160rpx)","fontSize":"28rpx","borderStyle":"solid","height":"88rpx"}' :disabled="ro.dizhi" v-model="ruleForm.dizhi" placeholder="地址"></input>
+			</view>
+			<view :style='{"padding":"0 40rpx","boxShadow":"0 0 0px rgba(0,0,0,.3)","backgroundColor":"rgba(255, 255, 255, 1)","borderColor":"#ccc","margin":"0 0 20rpx 0","borderRadius":"0","borderWidth":"0","width":"100%","borderStyle":"solid","height":"108rpx"}' class="cu-form-group select">
+				<view :style='{"padding":"0","boxShadow":"0 0 16rpx rgba(0,0,0,0)","margin":"0","borderColor":"#ccc","backgroundColor":"rgba(0,0,0,0)","color":"rgba(161, 161, 161, 1)","textAlign":"center","borderRadius":"0","borderWidth":"0","width":"160rpx","lineHeight":"80rpx","fontSize":"28rpx","borderStyle":"solid"}' class="title">注册时间</view>
+				<picker mode="date" :value="ruleForm.zhuceshijian" @change="zhuceshijianChange">
+					<view :style='{"padding":"0 30rpx","boxShadow":"0 0 0px rgba(0,0,0,.6) inset","margin":"0","backgroundColor":"rgba(255, 255, 255, 0)","borderColor":"rgba(0,0,0,.6)","color":"rgba(161, 161, 161, 1)","textAlign":"left","borderRadius":"16rpx","borderWidth":"2rpx","width":"calc(100% - 160rpx)","lineHeight":"80rpx","fontSize":"28rpx","borderStyle":"solid"}' class="uni-input">{{ruleForm.zhuceshijian?ruleForm.zhuceshijian:"请选择注册时间"}}</view>
+				</picker>
+			</view>
+			<view :style='{"padding":"0 40rpx","boxShadow":"0 0 0px rgba(0,0,0,.3)","margin":"0 0 20rpx 0","borderColor":"#ccc","backgroundColor":"rgba(255, 255, 255, 1)","borderRadius":"0","borderWidth":"0","width":"100%","borderStyle":"solid","height":"108rpx"}' class="cu-form-group">
+				<view :style='{"padding":"0","boxShadow":"0 0 16rpx rgba(0,0,0,0)","margin":"0","borderColor":"#ccc","backgroundColor":"rgba(0,0,0,0)","color":"rgba(161, 161, 161, 1)","textAlign":"center","borderRadius":"0","borderWidth":"0","width":"160rpx","lineHeight":"80rpx","fontSize":"28rpx","borderStyle":"solid"}' class="title">注册金额</view>
+				<input :style='{"padding":"0 30rpx","boxShadow":"0 0 0px rgba(0,0,0,.6) inset","margin":"0","borderColor":"rgba(0,0,0,.6)","backgroundColor":"rgba(255, 255, 255, 0)","color":"rgba(161, 161, 161, 1)","textAlign":"left","borderRadius":"16rpx","borderWidth":"2rpx","width":"calc(100% - 160rpx)","fontSize":"28rpx","borderStyle":"solid","height":"88rpx"}' :disabled="ro.zhucejine" v-model="ruleForm.zhucejine" placeholder="注册金额"></input>
+			</view>
+			<view :style='{"padding":"0 40rpx","boxShadow":"0 0 0px rgba(0,0,0,.3)","margin":"0 0 20rpx 0","borderColor":"#ccc","backgroundColor":"rgba(255, 255, 255, 1)","borderRadius":"0","borderWidth":"0","width":"100%","borderStyle":"solid","height":"108rpx"}' class="cu-form-group">
+				<view :style='{"padding":"0","boxShadow":"0 0 16rpx rgba(0,0,0,0)","margin":"0","borderColor":"#ccc","backgroundColor":"rgba(0,0,0,0)","color":"rgba(161, 161, 161, 1)","textAlign":"center","borderRadius":"0","borderWidth":"0","width":"160rpx","lineHeight":"80rpx","fontSize":"28rpx","borderStyle":"solid"}' class="title">联系电话</view>
+				<input :style='{"padding":"0 30rpx","boxShadow":"0 0 0px rgba(0,0,0,.6) inset","margin":"0","borderColor":"rgba(0,0,0,.6)","backgroundColor":"rgba(255, 255, 255, 0)","color":"rgba(161, 161, 161, 1)","textAlign":"left","borderRadius":"16rpx","borderWidth":"2rpx","width":"calc(100% - 160rpx)","fontSize":"28rpx","borderStyle":"solid","height":"88rpx"}' :disabled="ro.lianxidianhua" v-model="ruleForm.lianxidianhua" placeholder="联系电话"></input>
+			</view>
+			<view :style='{"padding":"0 40rpx","boxShadow":"0 0 0px rgba(0,0,0,.3)","backgroundColor":"rgba(255, 255, 255, 1)","borderColor":"#ccc","margin":"0 0 20rpx 0","borderRadius":"0","borderWidth":"0","width":"100%","borderStyle":"solid","height":"108rpx"}' class="cu-form-group" @tap="fengmianTap">
+				<view :style='{"padding":"0","boxShadow":"0 0 16rpx rgba(0,0,0,0)","margin":"0","borderColor":"#ccc","backgroundColor":"rgba(0,0,0,0)","color":"rgba(161, 161, 161, 1)","textAlign":"center","borderRadius":"0","borderWidth":"0","width":"160rpx","lineHeight":"80rpx","fontSize":"28rpx","borderStyle":"solid"}' class="title">封面</view>
+				<view class="right-input" style="padding:0">
+					<image :style='{"padding":"0","boxShadow":"0 0 0px rgba(0,0,0,.3)","margin":"0 atuo","borderColor":"#ccc","backgroundColor":"rgba(0,0,0,0)","borderRadius":"100%","borderWidth":"2rpx","width":"88rpx","borderStyle":"solid","height":"88rpx"}' class="avator" v-if="ruleForm.fengmian" :src="baseUrl+ruleForm.fengmian" mode="aspectFill"></image>
+					<image :style='{"padding":"0","boxShadow":"0 0 0px rgba(0,0,0,.3)","margin":"0 atuo","borderColor":"#ccc","backgroundColor":"rgba(0,0,0,0)","borderRadius":"100%","borderWidth":"2rpx","width":"88rpx","borderStyle":"solid","height":"88rpx"}' class="avator" v-else src="../../static/gen/upload.png" mode="aspectFill"></image>
+				</view>
+			</view>
+			
+			<!-- 否 -->
+ 
+
+			
+			<view :style='{"padding":"0 40rpx","boxShadow":"0 0 0px rgba(0,0,0,.3)","backgroundColor":"rgba(255, 255, 255, 1)","borderColor":"#ccc","margin":"0 0 20rpx 0","borderRadius":"0","borderWidth":"0","width":"100%","borderStyle":"solid","height":"308rpx"}' class="cu-form-group">
+				<view :style='{"padding":"0","boxShadow":"0 0 0px rgba(0,0,0,0)","margin":"0","borderColor":"#ccc","backgroundColor":"rgba(0,0,0,0)","color":"rgba(161, 161, 161, 1)","textAlign":"center","borderRadius":"0","borderWidth":"0","width":"160rpx","lineHeight":"80rpx","fontSize":"28rpx","borderStyle":"solid"}' class="title">详情</view>
+				<textarea :style='{"padding":"20rpx","boxShadow":"0 0 0px rgba(0,0,0,.6) inset","margin":"0","backgroundColor":"rgba(255, 255, 255, 0)","borderColor":"rgba(0,0,0,.6)","borderRadius":"16rpx","color":"rgba(161, 161, 161, 1)","borderWidth":"2rpx","width":"calc(100% - 160rpx)","fontSize":"28rpx","borderStyle":"solid","height":"280rpx"}' v-model="ruleForm.xiangqing" placeholder="详情"></textarea>
+			</view>
+			
+			<view class="btn" >
+				<button :style='{"padding":"0","boxShadow":"0 0 16rpx rgba(0,0,0,0) inset","margin":"0","backgroundColor":"rgba(255, 105, 18, 1)","borderColor":"rgba(255, 105, 18, 1)","borderRadius":"80rpx","color":"#fff","borderWidth":"1","width":"300rpx","fontSize":"28rpx","borderStyle":"solid","height":"80rpx"}' @tap="onSubmitTap" class="bg-red">提交</button>
+			</view>
+		</form>
+
+			
+	</view>
+</template>
+
+<script>
+	import wPicker from "@/components/w-picker/w-picker.vue";
+
+	export default {
+		data() {
+			return {
+				cross:'',
+				ruleForm: {
+				gongsimingcheng: '',
+				dizhi: '',
+				zhuceshijian: '',
+				zhucejine: '',
+				xiangqing: '',
+				lianxidianhua: '',
+				fengmian: '',
+				},
+				// 登陆用户信息
+				user: {},
+                                ro:{
+                                   gongsimingcheng : false,
+                                   dizhi : false,
+                                   zhuceshijian : false,
+                                   zhucejine : false,
+                                   xiangqing : false,
+                                   lianxidianhua : false,
+                                   fengmian : false,
+                                },
+			}
+		},
+		components: {
+			wPicker
+		},
+		computed: {
+			baseUrl() {
+				return this.$base.url;
+			},
+
+
+
+		},
+		async onLoad(options) {
+			let table = uni.getStorageSync("nowTable");
+			// 获取用户信息
+			let res = await this.$api.session(table);
+			this.user = res.data;
+			
+			// ss读取
+
+
+
+			// 如果有登陆，获取登陆后保存的userid
+			this.ruleForm.userid = uni.getStorageSync("userid")
+			if (options.refid) {
+				// 如果上一级页面传递了refid，获取改refid数据信息
+				this.ruleForm.refid = options.refid;
+				this.ruleForm.nickname = uni.getStorageSync("nickname");
+			}
+			// 如果是更新操作
+			if (options.id) {
+				this.ruleForm.id = options.id;
+				// 获取信息
+				res = await this.$api.info(`gongsijieshao`, this.ruleForm.id);
+				this.ruleForm = res.data;
+			}
+			// 跨表
+			this.cross = options.cross;
+			if(options.cross){
+				var obj = uni.getStorageSync('crossObj');
+				for (var o in obj){
+					if(o=='gongsimingcheng'){
+					this.ruleForm.gongsimingcheng = obj[o];
+					this.ro.gongsimingcheng = true;
+					continue;
+					}
+					if(o=='dizhi'){
+					this.ruleForm.dizhi = obj[o];
+					this.ro.dizhi = true;
+					continue;
+					}
+					if(o=='zhuceshijian'){
+					this.ruleForm.zhuceshijian = obj[o];
+					this.ro.zhuceshijian = true;
+					continue;
+					}
+					if(o=='zhucejine'){
+					this.ruleForm.zhucejine = obj[o];
+					this.ro.zhucejine = true;
+					continue;
+					}
+					if(o=='xiangqing'){
+					this.ruleForm.xiangqing = obj[o];
+					this.ro.xiangqing = true;
+					continue;
+					}
+					if(o=='lianxidianhua'){
+					this.ruleForm.lianxidianhua = obj[o];
+					this.ro.lianxidianhua = true;
+					continue;
+					}
+					if(o=='fengmian'){
+					this.ruleForm.fengmian = obj[o];
+					this.ro.fengmian = true;
+					continue;
+					}
+				}
+			}
+			this.styleChange()
+		},
+		methods: {
+			styleChange() {
+				this.$nextTick(()=>{
+					// document.querySelectorAll('.app-update-pv .cu-form-group .uni-input-input').forEach(el=>{
+					//   el.style.backgroundColor = this.addUpdateForm.input.content.backgroundColor
+					// })
+				})
+			},
+
+			// 多级联动参数
+
+			zhuceshijianChange(e) {
+				this.ruleForm.zhuceshijian = e.target.value;
+				this.$forceUpdate();
+			},
+
+
+
+			fengmianTap() {
+				let _this = this;
+				this.$api.upload(function(res) {
+					_this.ruleForm.fengmian = 'upload/' + res.file;
+					_this.$forceUpdate();
+					_this.$nextTick(()=>{
+						_this.styleChange()
+					})
+				});
+			},
+
+			getUUID () {
+				return new Date().getTime();
+			},
+			async onSubmitTap() {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//跨表计算判断
+				if(this.ruleForm.zhucejine&&(!this.$validate.isIntNumer(this.ruleForm.zhucejine))){
+					this.$utils.msg(`注册金额应输入整数`);
+					return
+				}
+				if(this.ruleForm.lianxidianhua&&(!this.$validate.isMobile(this.ruleForm.lianxidianhua))){
+					this.$utils.msg(`联系电话应输入手机格式`);
+					return
+				}
+				//更新跨表属性
+			       var crossuserid;
+			       var crossrefid;
+			       var crossoptnum;
+				if(this.cross){
+					var statusColumnName = uni.getStorageSync('statusColumnName');
+					var statusColumnValue = uni.getStorageSync('statusColumnValue');
+					if(statusColumnName!='') {
+						var obj = uni.getStorageSync('crossObj');
+						if(!statusColumnName.startsWith("[")) {
+							for (var o in obj){
+								if(o==statusColumnName){
+									obj[o] = statusColumnValue;
+								}
+
+							}
+							var table = uni.getStorageSync('crossTable');
+							await this.$api.update(`${table}`, obj);
+						} else {
+						       crossuserid=Number(uni.getStorageSync('userid'));
+						       crossrefid=obj['id'];
+						       crossoptnum=uni.getStorageSync('statusColumnName');
+						       crossoptnum=crossoptnum.replace(/\[/,"").replace(/\]/,"");
+						}
+					}
+				}
+				if(crossrefid && crossuserid) {
+					this.ruleForm.crossuserid=crossuserid;
+					this.ruleForm.crossrefid=crossrefid;
+					let params = {
+						page: 1,
+						limit:10,
+						crossuserid:crossuserid,
+						crossrefid:crossrefid,
+					}
+					let res = await this.$api.list(`gongsijieshao`, params);
+					if (res.data.total >= crossoptnum) {
+						this.$utils.msg(uni.getStorageSync('tips'));
+						return false;
+					} else {
+                //跨表计算
+						if(this.ruleForm.id){
+							await this.$api.update(`gongsijieshao`, this.ruleForm);
+						}else{
+							await this.$api.add(`gongsijieshao`, this.ruleForm);
+						}
+						this.$utils.msgBack('提交成功');
+					}
+				} else {
+                //跨表计算
+					if(this.ruleForm.id){
+						await this.$api.update(`gongsijieshao`, this.ruleForm);
+					}else{
+						await this.$api.add(`gongsijieshao`, this.ruleForm);
+					}
+					this.$utils.msgBack('提交成功');
+				}
+			},
+			optionsChange(e) {
+				this.index = e.target.value
+			},
+			bindDateChange(e) {
+				this.date = e.target.value
+			},
+			getDate(type) {
+				const date = new Date();
+				let year = date.getFullYear();
+				let month = date.getMonth() + 1;
+				let day = date.getDate();
+				if (type === 'start') {
+					year = year - 60;
+				} else if (type === 'end') {
+					year = year + 2;
+				}
+				month = month > 9 ? month : '0' + month;;
+				day = day > 9 ? day : '0' + day;
+				return `${year}-${month}-${day}`;
+			},
+			toggleTab(str) {
+				this.$refs[str].show();
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.container {
+		padding: 20upx;
+	}
+	
+	.content:after {
+		position: fixed;
+		top: 0;
+		right: 0;
+		left: 0;
+		bottom: 0;
+		content: '';
+		background-image: url(http://codegen.caihongy.cn/20220212/27765cb61e8943d18c29beb442e4374f.png);
+		background-attachment: fixed;
+		background-size: cover;
+		background-position: center;
+	}
+
+	textarea {
+		border: 1upx solid #EEEEEE;
+		border-radius: 20upx;
+		padding: 20upx;
+	}
+
+	.title {
+		width: 180upx;
+	}
+
+	.avator {
+		width: 150upx;
+		height: 60upx;
+	}
+
+	.right-input {
+		flex: 1;
+		text-align: left;
+		padding: 0 24upx;
+	}
+	
+	.cu-form-group.active {
+		justify-content: space-between;
+	}
+	
+	.btn {
+	  display: flex;
+	  align-items: center;
+	  justify-content: center;
+	  flex-wrap: wrap;
+	  padding: 20upx 0;
+	}
+	
+	.cu-form-group {
+		padding: 0 24upx;
+		background-color: transparent;
+		min-height: inherit;
+	}
+	
+	.cu-form-group+.cu-form-group {
+		border: 0;
+	}
+	
+	.cu-form-group uni-input {
+		padding: 0 30upx;
+	}
+	
+	.uni-input {
+		padding: 0 30upx;
+	}
+	
+	.cu-form-group uni-textarea {
+		padding: 30upx;
+		margin: 0;
+	}
+	
+	.cu-form-group uni-picker::after {
+		line-height: 80rpx;
+	}
+	
+	.select .uni-input {
+		line-height: 80rpx;
+	}
+	
+	.input .right-input {
+		line-height: 88rpx;
+	}
+</style>
